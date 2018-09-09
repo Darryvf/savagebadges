0dc88b1852b1f0d71afa6420ffd66a66f789dd1f
Darrell VanFleet <dasmvanfleet@msn.com>
2018-09-08 22:32:23 -0600

    initial commit

---
+++ Savage Badges/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Savage Badges
+//
+//  Created by Darrell Vanfleet on 9/8/18.
+//  Copyright © 2018 Darrell Vanfleet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
