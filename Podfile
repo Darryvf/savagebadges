7a33089b97b6f6d7af96131eca795cfa96782cc1
Darrell VanFleet <dasmvanfleet@msn.com>
2018-09-11 21:02:50 -0600

    Auth0 in and login/logout

---
+++ Podfile
@@ -0,0 +1,5 @@
+use_frameworks!
+
+target 'savagebadges' do
+  pod 'Auth0', '~> 1.0'
+end
--- savagebadges/ViewControllers/LoginViewController.swift
+++ savagebadges/ViewControllers/LoginViewController.swift
@@ -7,9 +7,12 @@
 //
 
 import UIKit
+import Auth0
+
 
 class LoginViewController: UIViewController {
-
+    var credentials:Credentials?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -20,16 +23,48 @@ class LoginViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func showLoginController(_ sender: UIButton) {
+        guard let clientInfo = plistValues(bundle: Bundle.main) else {return}
+        Auth0
+            .webAuth()
+            .scope("openid profile")
+            .audience("https://savagebadges.auth0.com/userinfo")
+            .start {
+                switch $0 {
+                case .failure(let error):
+                    // Handle the error
+                    print("Error: \(error)")
+                case .success(let credentials):
+                    guard let accessToken = credentials.accessToken, let idToken = credentials.idToken else { return }
+                    self.credentials = credentials
+                    print("Credentials: \(credentials)")
+                    print("Credentials access token: \(credentials.accessToken)")
+                }
+            }
     }
-    */
+    
+    @IBAction func Logout(_ sender: Any) {
+        self.credentials = nil
+    }
+
+    func plistValues(bundle: Bundle) -> (clientId: String, domain: String)? {
+        guard
+            let path = bundle.path(forResource: "Auth0", ofType: "plist"),
+            let values = NSDictionary(contentsOfFile: path) as? [String: Any]
+            else {
+                print("Missing Auth0.plist file with 'ClientId' and 'Domain' entries in main bundle!")
+                return nil
+        }
+        
+        guard
+            let clientId = values["ClientId"] as? String,
+            let domain = values["Domain"] as? String
+            else {
+                print("Auth0.plist file at \(path) is missing 'ClientId' and/or 'Domain' entries!")
+                print("File currently has the following entries: \(values)")
+                return nil
+        }
+        return (clientId: clientId, domain: domain)
+    }
 
 }
